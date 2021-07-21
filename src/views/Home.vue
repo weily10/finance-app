7cdbcbbea4040ba3487a8bbf52ca5693821b63e6
weily10 <w3ily10@gmail.com>
2021-07-21 22:11:05 +0800

    basic ui done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-text-field label="search stocks" v-model="search"></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        {{stockName}}
+      </v-col>
+      <v-col>
+        {{price}}
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-text-field
+          label="shares"
+          v-model="shares"
+          type="number"
+        ></v-text-field>
+      </v-col>
+
+    </v-row>
+    <v-row>
+       <v-col>
+       Dividend earned: {{dividend}} <br>
+       You get {{dividendMonth}} per month
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '../components/HelloWorld'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+  data() {
+    return {
+      shares: 0,
+      stockName:'',
+      price:'$'+0,
+      dividend:0,
+      dividendMonth:0
+    };
+  },
+};
 </script>
