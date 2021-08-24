bb301c60b0a3efbcf3663324bd30a6741a1cdc7d
weily huang <w3ily10@gmail.com>
2021-08-24 17:50:06 +0800

    save

--- src/views/FreeCalculator.vue
+++ src/views/FreeCalculator.vue
@@ -8,16 +8,23 @@
             v-model="assetValue"
             type="number"
           ></v-text-field>
-        
+
           <v-text-field
             label="dividend yield"
             v-model="divyield"
             type="number"
             suffix="%"
           ></v-text-field>
-                <v-btn color="blue" dark depressed block @click="addData()">ok</v-btn>
-          </div>
-    
+          <v-btn
+            color="blue"
+            dark
+            depressed
+            block
+            @click="addData(assetValue, divyield)"
+            >ok</v-btn
+          >
+        </div>
+
         <!-- <v-col>
           Total Investment:
           {{ Intl.NumberFormat().format(assetValue * shares * 1000) }} <br />
@@ -34,12 +41,26 @@
       </v-card>
     </v-dialog>
     <v-list three-line>
-      <div>
-      <div align="center">no data</div>
-      <div align="center">
-        <v-btn color="blue" depressed dark @click="dialog1 = true">add</v-btn>
+   
+      <v-toolbar depressed class="elevation-0">
+        <h3>Assets</h3>
+        <v-spacer></v-spacer>
+        <v-btn color="blue"  rounded depressed   dark @click="dialog1 = true"
+          > <v-icon>mdi-plus</v-icon><v-spacer></v-spacer>  add </v-btn>
+      </v-toolbar>
+          <div v-show="assets.length < 0">
+        <div align="center">no data</div>
+       
       </div>
-      </div>
+      <template v-for="(asset, index) in assets">
+        <div :key="asset + index">
+          <div class="flex pt-3">
+            <div class="pa-3">value: $ {{ asset.assetValue }}</div>
+            <div class="pa-3">dividend: {{ asset.divyield }}%</div>
+          </div>
+          <v-divider></v-divider>
+        </div>
+      </template>
     </v-list>
   </v-container>
 </template>
@@ -49,6 +70,7 @@ export default {
   components: {},
   data() {
     return {
+      assets: [],
       dialog1: false,
       quotes: [],
       divyield: 0.0,
@@ -61,12 +83,22 @@ export default {
       search: null,
     };
   },
-  methods:{
-    addData(){
-      this.dialog1=false
-    }
+  methods: {
+    addData(assetValue, divyield) {
+      this.dialog1 = false;
+      // console.log(assetValue,divyield);
+      this.assets.push({
+        assetValue: Intl.NumberFormat().format(assetValue),
+        divyield: divyield,
+      });
+    },
   },
 
   mounted() {},
 };
 </script>
+<style scoped>
+.flex {
+  display: flex;
+}
+</style>
