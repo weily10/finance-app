67974337455c8e6374220c26b3db5e2171b6bcca
weily10 <w3ily10@gmail.com>
2025-07-18 18:00:15 +0800

    add modal

---
+++ src/components/modals/BaseModal.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+    show: boolean
+}>()
+</script>
+
+<template>
+    <Teleport to="#modals">
+
+        <div v-if="show" class="">
+
+            <div class="fixed inset-0 bg-gray-900 opacity-40"></div>
+
+
+            <div class="fixed inset-0 flex items-center justify-center">
+                <div class="bg-white text-black">
+                    <slot></slot>
+                </div>
+            </div>
+        </div>
+    </Teleport>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
+import BaseModal from '../components/modals/BaseModal.vue'
+
+const showModal = ref(false)
 
 </script>
 
 <template>
     <div class="text-start">
-        <h1>
+        <h1 class="text-2xl">
             Input you numbers
         </h1>
 
@@ -12,17 +17,29 @@
             <div class="w-full max-w-sm min-w-[200px]">
                 <label for="">dividend</label>
                 <input type="input"
-                    class="w-full bg-white  placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+                    class="w-full bg-white  placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300    "
                     placeholder="Type here...">
             </div>
             <div class=" mt-3">
                 <label for="">dividend</label>
                 <div class="w-full max-w-sm min-w-[200px]">
                     <input type="input"
-                        class="w-full bg-white  placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+                        class="w-full bg-white  placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300   "
                         placeholder="Type here...">
                 </div>
             </div>
+            <button type="button" class="mt-3 w-full" @click="showModal = true"> Add new card</button>
+            <BaseModal :show="showModal">
+                <div class="p-4">
+                    <div class="text-lg">Stock info</div>
+
+                    <div class="py-2 text-sm">Click to close:</div>
+
+                    <button type="button" class="bg-indigo-200 px-3 py-1 font-medium" @click="showModal = false">
+                        Hide modal
+                    </button>
+                </div>
+            </BaseModal>
         </form>
     </div>
 </template>
