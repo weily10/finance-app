b6753032688353afc75c9595d0043f81aa6301ff
weily10 <w3ily10@gmail.com>
2025-07-22 15:06:11 +0800

    add modal

--- src/components/modals/BaseModal.vue
+++ src/components/modals/BaseModal.vue
@@ -6,17 +6,18 @@ defineProps<{
 
 <template>
     <Teleport to="#modals">
+        <Transition enter-active-class="transition duration-300 ease-out" enter-from-class="opacity-0 "
+            enter-to-class="opacity-100  " leave-active-class="transition duration-200 ease-in"
+            leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0  ">
+            <div v-if="show" class="fixed inset-0 z-50 flex items-center justify-center">
+                <!-- Backdrop -->
+                <div class="absolute inset-0 bg-black opacity-40"></div>
 
-        <div v-if="show" class="">
-
-            <div class="fixed inset-0 bg-gray-900 opacity-40"></div>
-
-
-            <div class="fixed inset-0 flex items-center justify-center">
-                <div class="bg-white text-black">
+                <!-- Modal -->
+                <div class="relative bg-white text-black shadow-lg rounded-lg p-6">
                     <slot></slot>
                 </div>
             </div>
-        </div>
+        </Transition>
     </Teleport>
 </template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -30,14 +30,32 @@ const showModal = ref(false)
             </div>
             <button type="button" class="mt-3 w-full" @click="showModal = true"> Add new card</button>
             <BaseModal :show="showModal">
-                <div class="p-4">
-                    <div class="text-lg">Stock info</div>
+                <div class="text-lg">Stock info</div>
+                <div class="mt-5">
 
-                    <div class="py-2 text-sm">Click to close:</div>
+                    <div>
+                        <div>
+                            <label for="">divdend</label>
+                            <input id="" type="input"
+                                class="w-full bg-white  placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300   "
+                                placeholder="Type here...">
+                        </div>
+                        <div class="mt-2">
+                            <label for="">info2</label>
+                            <input type="input"
+                                class="w-full bg-white  placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300   "
+                                placeholder="Type here...">
+                        </div>
 
-                    <button type="button" class="bg-indigo-200 px-3 py-1 font-medium" @click="showModal = false">
-                        Hide modal
-                    </button>
+                    </div>
+                    <div class="flex gap-3 mt-5 justify-end">
+                        <button type="button" class="bg-indigo-200 px-3 py-1 font-medium" @click="showModal = false">
+                            cancel
+                        </button>
+                        <button type="button" class="bg-indigo-200 px-3 py-1 font-medium" @click="showModal = false">
+                            add
+                        </button>
+                    </div>
                 </div>
             </BaseModal>
         </form>
