16e2c6f4f7161cfc19ec25222d12f552592565f1
weily huang <w3ily10@gmail.com>
2025-08-05 22:41:29 +0800

    add register

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import Login from '../pages/Login.vue'
+import Register from '../pages/Register.vue'
  
 
 
@@ -14,6 +15,11 @@ const routes = [
         path:"/login",
         name: "Login",
         component: Login,
+    },
+    {
+        path:"/register",
+        name: "Register",
+        component: Register,
     }
     
     
