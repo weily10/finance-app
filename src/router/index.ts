dda97be5a03f9041e8724cdff5f4b59b660f0743
weily huang <w3ily10@gmail.com>
2025-08-05 23:19:17 +0800

    login register page done

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../pages/Home.vue'
-import Login from '../pages/Login.vue'
-import Register from '../pages/Register.vue'
- 
-
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../pages/Home.vue";
+import Login from "../pages/Login.vue";
+import Register from "../pages/Register.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: Home,
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: {
+      requiresAuth: true,
     },
-    {
-        path:"/login",
-        name: "Login",
-        component: Login,
-    },
-    {
-        path:"/register",
-        name: "Register",
-        component: Register,
-    }
-    
-    
-]
-
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+];
 
 const history = createWebHistory();
 const router = createRouter({
-    history,
-    routes
-})
+  history,
+  routes,
+});
 
- 
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("token"); // Or use Vuex, Pinia, etc.
 
-export default router
+  if (to.meta.requiresAuth && !token) {
+    next({ name: "Login" }); // Redirect to login if not authenticated
+  } else {
+    next(); // Proceed normally
+  }
+});
+
+export default router;
