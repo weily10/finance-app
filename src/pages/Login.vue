4527d398deff65448650aa5364a68f6dd4119f70
weily10 <w3ily10@gmail.com>
2025-08-08 17:55:09 +0800

    Update Login.vue

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -53,14 +53,13 @@ function onToastClosed() {
       <h1 class="text-center font-bold text-xl">Login</h1>
       <div class="flex justify-center">
         <div class="p-8 border border-gray-200 mt-3 rounded-md md:w-100">
-          <form>
+          <form @submit.prevent="login">
             <Input label="username" v-model:model="credentials.email"
               :placeholder="'enter your username or email'"></Input>
             <Input type="password" label="password" v-model:model="credentials.password" class="mt-3"
               :placeholder="'minimun 8 digits'"></Input>
             <div class="mt-4">
-              <Button label="Login" :type="'button'" :disabled="!credentials.password || !credentials.email"
-                @onClick="login"></Button>
+              <Button label="Login" :type="'submit'" :disabled="!credentials.password || !credentials.email"></Button>
               <p v-show="error">{{ error }}</p>
             </div>
           </form>
