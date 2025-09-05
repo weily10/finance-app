afb20eeb76f96701bcc86c51e465eda1393d3a3d
weily10 <w3ily10@gmail.com>
2025-09-05 17:13:19 +0800

    Update Toast.vue

--- src/components/modals/Toast.vue
+++ src/components/modals/Toast.vue
@@ -62,7 +62,7 @@ watch(
                     </div>
                     <div class="ms-3 text-sm font-normal me-3">{{ props.message }}</div>
                     <button type="button"
-                        class="cursor-pointer ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                        class="cursor-pointer ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8"
                         data-dismiss-target="#toast-default" aria-label="Close" @click="$emit('close')">
                         <span class="material-symbols-outlined !text-sm"> close </span>
                     </button>
